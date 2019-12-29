a61807b6281f28d1f46b435996283615ada1d61c
hello1905 <2069347026@qq.com>
2019-12-30 00:37:15 +0800

    19

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
+  
 const express = require('express')
 const app = express()
-const bodyParset = require("body-parser")
+const bodyParser = require("body-parser")
 
 app.use(express.static('.'))
-app.use(bodyParset.json());
-app.use(bodyParset.urlencoded({ extended: false }));
-app.post('/formBuilder', function (req, res) {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('../uploads'))
+const multer = require('multer');
+var storage = multer.diskStorage({
+    destination:function (req, file, cb) {
+        cb(null, '../uploads')
+    },
+    filename: function (req, file, cb) {
+        var fileFormat = (file.originalname).split(".");
+        cb(null, file.fieldname + '-' + Date.now() + "." + fileFormat[fileFormat.length - 1]);
+    }
+});
+let upload = multer({ storage: storage })
+app.post('/upload',upload.single('file'),function(req,res,next){
+    var file=req.file;
+    console.log("original file name is "+file.originalname);
+    console.log("file name is" + file.filename);
+    res.json('/'+file.filename);
+})
+app.post('/formBuilder',function (req,res) {
     console.log(req.body)
     res.send(req.body)
 })
+
 let ajaxData = []
 let count = 0
 app.post('/ajax', function (req, res) {
@@ -33,13 +53,11 @@ app.get('/ajax', function (req, res) {
     let size = 5
     let maxpage = Math.ceil(ajaxData.length/size)
     result={data:ajaxData.slice((page-1)*size,page*size),
-    maxpage:maxpage
+    maxPage:maxpage
 }
 res.json(result)
 })
-
-
-app.listen(8080, () =>console.log('node express 服务器已启动，监听端口：8080'))
+app.listen(8080, () => console.log('node express 服务器已启动，监听端口:8080'))
 
 const openDefaultBrowser = function (url) {
     var exec = require('child_process').exec;
@@ -47,11 +65,40 @@ const openDefaultBrowser = function (url) {
         case "darwin":
             exec('open ' + url);
             break;
-        case "win32":
-            exec('start '+ url);
-            break;
-        default:
-            exec('xdg-open', [url]);
+            case "win32":
+                exec('start ' + url);
+                break;
+                default:
+                    exec('xdg-open', [url]);
     }
 }
 openDefaultBrowser('http://localhost:8080')
+
+var ws = require("nodejs-websocket")
+let id=0
+
+var server = ws.createServer(function (conn) {
+    id++
+    conn.name = "p"+id
+    broadcast(server,'有新人加入.')
+    conn.on("text", function (str) {
+        if(str.slice(0,9)=='nickname|'){
+            conn.name=str.split('|')[1]
+            broadcast(server,conn.name+'上线了。')
+            return
+        }
+        broadcast(server,conn.name+':'+str)
+    })
+    conn.on('connect',function(){
+        conn.name = "name"
+    })
+    conn.on("close", function (code, reason) {
+        console.log("Connection closed")
+    })
+}).listen(8081,()=>console.log('socket server listening on:8081'))
+
+function broadcast(server, msg) {
+    server.connections.forEach(function (conn) {
+        conn.sendText(msg)
+    })
+}
