96db924fdfa11ad0ff44fe67be546299fc2c57ba
hello1905 <2069347026@qq.com>
2020-01-02 15:38:42 +0800

    21

--- work21/index.js
+++ work21/index.js
@@ -1,31 +1,37 @@
 $(function(){
     var i=0;
-    var timer=null;
-    var delay=1000;
-    var width=1420;
-    var speed=400;
-    var firstimg=$('.hot li').first().clone();
-    $('.hot').append(firstimg).width($('.hot  li').length*width);
-    timer=setInterval(imgChang,delay);
+    var timer= null;
+    var delay=1500;
+    var width = 1420;
+    var speed= 400;
+
+    var firstimg = $('.hot li').first().clone();
+    $('.hot').append(firstimg).width($('.hot li').length * width);
+
+    timer=setInterval(imgChange,delay);
+
     $('.banner').hover(function(){
         clearInterval(timer);
     },function(){
-        timer=setInterval(imgChang,delay);
+        timer = setInterval(imgChange,delay);
     });
+
     $('.dot li').mouseover(function(){
-        i=$(this).index();
+        i = $(this).index();
         $('.hot').stop().animate({
-            left:-i*width
+            left:-i * width
         },200);
         dotChange();
     });
+
     $('.banner').hover(function(){
         $('.arrow').show();
     },function(){
-        $('.arrow').hide()
+        $('.arrow').hide();
     });
+
     $('.next').click(function(){
-        imgChang();
+        imgChange()
     });
     $('.prev').click(function(){
         --i;
@@ -34,35 +40,35 @@ $(function(){
             $('.hot').css({
                 left:-($('.hot li').length-1)*width
             });
-
         }
         $('.hot').stop().animate({
-            left:-i * width
+            left:-i*width
         },speed);
         dotChange();
-    })
-    function imgChang(){
+    });
+    function imgChange(){
         ++i;
         isCrack();
         dotChange();
     }
+
     function isCrack(){
-        if(i==$('.hot li').length){
+        if(i == $('.hot li').length){
             i=1;
             $('.hot').css({
-                left:0
-            })
+                left: 0
+            });
         }
         $('.hot').stop().animate({
             left:-i*width
         },speed);
     }
-   function dotChange(){
-       if(i==$('.hot li').length-1){
-           $('.dot li').eq(0).addClass('on').siblings().removerClass('on');
-
-       }else{
-           $('.dot li').eq(i).addClass('on').siblings().removerClass('on')
-       }
-   }
-})
+    
+    function dotChange(){
+        if(i == $('.hot li').length-1){
+            $('.dot li').eq(0).addClass('on').siblings().removeClass('on');
+        }else{
+            $('.dot li').eq(i).addClass('on').siblings().removeClass('on');
+        }
+    }
+});
